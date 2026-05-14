35b70caa0de4bd66e4e530b406d1d9208e5276ad
Panchal Chetan <yspanchal6@gmail.com>
2026-05-14 14:56:08 +0530

    Fixed Vite config for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,29 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig({
+  base: '/',
 
-  return {
-    base: '/portfolio/',
+  root: 'client',
 
-    root: 'client',
+  plugins: [react(), tailwindcss()],
 
-    plugins: [react(), tailwindcss()],
+  publicDir: 'public',
 
-    publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 
-    build: {
-      outDir: 'dist',
-      emptyOutDir: true,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './client/src'),
     },
+  },
 
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './client/src'),
-      },
-    },
-
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  };
+  server: {
+    hmr: process.env.DISABLE_HMR !== 'true',
+  },
 });
